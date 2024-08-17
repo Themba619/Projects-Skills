a093a1b52a54f5a22de2474775c47b4a8dbb11cd
themba <thembabiyela20@gmail.com>
2024-08-17 19:21:41 +0200

    first commit

---
+++ login/firebase_login/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import Credentials from './components/Credentials'
+
+function App() {
+  return (
+    <>
+      <Credentials />
+    </>
+  )
+}
+
+export default App
---
+++ login/firebase_login/src/components/Credentials.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import '../CSS/Credentials.css'
+
+function Credentials() {
+  return (
+    <div className='login_container'>
+        <h1 className='header'>Login page</h1>
+    </div>
+  )
+}
+
+export default Credentials
